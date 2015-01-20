3309631b4386afa9b3d8f7e2ee55bb5c2c48179d
homeway <homeway.xue@gmail.com>
2015-01-20 10:49:43 +0800

    create an app

---
+++ src/snowstorm_n2o_app.erl
@@ -0,0 +1,16 @@
+-module(snowstorm_n2o_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    snowstorm_n2o_sup:start_link().
+
+stop(_State) ->
+    ok.
